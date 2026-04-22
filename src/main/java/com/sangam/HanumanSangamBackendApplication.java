0742a224e51d2821c8191df6f74c31431668d580
ungaralamanikanta <ungaralamanikanta959@gmail.com>
2026-04-22 18:29:45 +0530

    Update HanumanSangamBackendApplication.java

--- src/main/java/com/sangam/HanumanSangamBackendApplication.java
+++ src/main/java/com/sangam/HanumanSangamBackendApplication.java
@@ -2,11 +2,12 @@ package com.sangam;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-public class HanumanSangamBackendApplication {
-
+@EnableScheduling  // ← Add this
+public class HanumanSangamApplication {
     public static void main(String[] args) {
-        SpringApplication.run(HanumanSangamBackendApplication.class, args);
+        SpringApplication.run(HanumanSangamApplication.class, args);
     }
 }
